4e89b1cf130c66caa8032fb38ff274bdc63dda11
Pangho297 <monanazzang@gmail.com>
2023-01-07 18:42:06 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default function App() {
+  const Title = styled.h1`
+    font-size: 32px;
+    color: #4e61ff;
+  `;
+
+  return (
+    <div>
+      <Title>로켓어퍼캇!!</Title>
+    </div>
+  );
+}
